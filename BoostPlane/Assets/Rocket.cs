0aeb080677845c998a8be07fe29e03e037e16c8a
Gokul Singh <62161468+Goki27@users.noreply.github.com>
2023-09-03 23:23:04 +0530

    Particle System
    added particle system for thrusting blasting and fixed some movement and level related bugs

--- BoostPlane/Assets/Rocket.cs
+++ BoostPlane/Assets/Rocket.cs
@@ -1,13 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour
 {
-    [SerializeField]private float rcsThrust = 10f;
+    [SerializeField] private float rcsThrust = 10f;
     [SerializeField] private float mainThrust = 100f;
     Rigidbody rigibody;
+    [SerializeField] AudioClip mainEngine;
+    [SerializeField] AudioClip death;
+    [SerializeField] AudioClip sucess;
+    [SerializeField] ParticleSystem MainEngineParticle;
+    [SerializeField] ParticleSystem deathParticle;
+    [SerializeField] ParticleSystem sucessParticle;
     AudioSource audio;
+    enum State { Alive,dying,tanscending}
+    State state= State.Alive;
     // Start is called before the first frame update
     void Start()
     {
@@ -30,20 +39,26 @@ public class Rocket : MonoBehaviour
 
         if (Input.GetKey(KeyCode.Space))
         {
-
-            rigibody.AddRelativeForce(Vector3.up*mainThrust);
-            if (!audio.isPlaying)
-            {
-                audio.Play();
-            }
-            print("Thrust rocket is thrusting");
-
+            ApplyThrust();
 
         }
         else
         {
             audio.Stop();
+            MainEngineParticle.Stop();
         }
+        
+    }
+
+    private void ApplyThrust()
+    {
+        rigibody.AddRelativeForce(Vector3.up * mainThrust);
+        if (!audio.isPlaying)
+        {
+            audio.PlayOneShot(mainEngine);
+        }
+        MainEngineParticle.Play();
+        print("Thrust rocket is thrusting");
     }
 
     //logic for movement of the player
@@ -66,24 +81,49 @@ public class Rocket : MonoBehaviour
             transform.Rotate(-Vector3.forward*rotationSpeed);
         }
         rigibody.freezeRotation = false;
-    }
+    } 
 
     private void OnCollisionEnter(Collision collision)
     {
+        if(state!=State.Alive) { return; }
         switch (collision.gameObject.tag)
         {
             case "Friendly":
-                print("Friendly");
                 break;
-            case "Fuel":
-                print("Fuel");
+            case "Finish":
+                SucessSOund();
                 break;
-
+            default:
+                DeathSOund();
+                break;
         }
     }
 
+    private void DeathSOund()
+    {
+        state = State.dying;
+        audio.Stop();
+        audio.PlayOneShot(death);
+        deathParticle.Play();
+        Invoke("LoadFirstLevel", 1f);
+    }
 
+    private void SucessSOund()
+    {
+        state = State.tanscending;
 
+        audio.Stop();
+        audio.PlayOneShot(sucess);
+        sucessParticle.Play();
+        Invoke("LoadNextLevel", 1f);
+    }
 
-
+    private  void LoadNextLevel()
+    {
+        SceneManager.LoadScene(1);
+    }
+    private void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(0);
+    }
 }
